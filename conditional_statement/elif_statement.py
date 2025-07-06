f723250900e59ee0abf7d3b1fbb88d5fb237b58c
jeyleekrane <ijeyleekrane@gmail.com>
2025-07-06 17:16:29 +0100

    elif_statement was added as well

---
+++ conditional_statement/elif_statement.py
@@ -0,0 +1,14 @@
+# simple else statement
+
+Account_Balance = 200000.00
+
+Amount_withdraw = float(
+    input("please enter the amount of money u want to withdraw: "))
+
+if Amount_withdraw > Account_Balance:
+    print("the amount of money u want to withdraw is not aviailable in your account")
+elif Amount_withdraw == Account_Balance:
+    print("After this withdral your account balance will be zero")
+else:
+    print("take your money and leave"
+          )
