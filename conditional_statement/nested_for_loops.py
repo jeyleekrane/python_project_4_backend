ac4663948acc2751a65345774e2bb8298f2a5507
jeyleekrane <ijeyleekrane@gmail.com>
2025-07-06 23:48:52 +0100

    nested for loop added

---
+++ conditional_statement/nested_for_loops.py
@@ -0,0 +1,8 @@
+names = ["Alhaji Sule", "Oyinbo", "Hajara",
+         "Jamila", "Hussien", "Hassan", "Halima",]
+surnames = ["Ben 10", "Khalifa", "Yunus", "Isa", "Musa", "Sadiq", "Baraka",]
+
+for name in names:
+    print(f"{name}")
+    for surname in surnames:
+        print(f"{surnames}")
