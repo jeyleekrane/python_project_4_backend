10b0c2bfb498124bddc4861fb462f77280687680
jeyleekrane <ijeyleekrane@gmail.com>
2025-07-06 16:48:48 +0100

    a simple prompt for age

---
+++ conditional_statement/If_statement.py
@@ -0,0 +1,6 @@
+# if statement for a username prompt
+
+name = int(input("Please enter your username"))
+DoB = int(input("please enter your date of birth"))
+if DoB > 30:
+    print("Your are to old for this application")
