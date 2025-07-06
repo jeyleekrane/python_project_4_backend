85b0178e46d7c73c0bde5282d836999aa1446c3c
jeyleekrane <ijeyleekrane@gmail.com>
2025-07-06 17:07:14 +0100

    if and if else done seperately

--- conditional_statement/If_statement.py
+++ conditional_statement/If_statement.py
@@ -1,6 +1,8 @@
 # if statement for a username prompt
 
-name = int(input("Please enter your username"))
-DoB = int(input("please enter your date of birth"))
+
+DoB = int(input("please enter your date of birth:  "))
+print(f"{DoB}")
+
 if DoB > 30:
-    print("Your are to old for this application")
+    print(f"{DoB} Your are to old for this application")
