34df6328f4bd5f34b1b381ac1898336117d938a7
jeyleekrane <ijeyleekrane@gmail.com>
2025-07-07 02:20:59 +0100

    list in python, methods like pop , delete, clear and append are been used

---
+++ list.py
@@ -0,0 +1,14 @@
+fruits = ["ornage", "omango", "banana", "grapes"]
+print("banana" in fruits)
+print(len(fruits))
+print(fruits[0:2])
+fruits.append("watermelon")
+print(fruits)
+fruits[0] = "fish"
+print(fruits)
+fruits.remove("fish")
+print(fruits)
+fruits.pop()
+print(fruits)
+print(fruits.clear())
+print("Python is just amazing to work with")
