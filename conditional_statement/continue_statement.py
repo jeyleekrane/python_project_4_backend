5474b26743f26ca4b85ed716a094e018c1f63571
jeyleekrane <ijeyleekrane@gmail.com>
2025-07-07 01:19:24 +0100

    Continue statement is used in this

---
+++ conditional_statement/continue_statement.py
@@ -0,0 +1,8 @@
+# this block of code will stop brake one the number is =3 by decrementing the value of the number
+
+number = 6
+while number > 0:
+    number = number-1
+    if number == 3:
+        break
+    print(number)
