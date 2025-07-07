4dd1e6d69e4432f7908a230672cb625974d14e4c
jeyleekrane <ijeyleekrane@gmail.com>
2025-07-07 12:12:59 +0100

    nested list is added up

---
+++ conditional_statement/nested_list.py
@@ -0,0 +1,8 @@
+nested_list = [1, 2, 3, 4, "h", [1, "k"], 6, ["apple", 2, [
+    "fish", "stew", "rice", "beans"]], ["white rice", "jellof rice", "bread"]]
+print(len(nested_list))
+print(nested_list[8])
+print(nested_list[8][2])
+
+fruits = ["apple", "mango", "bread"]
+print(fruits.sort())
